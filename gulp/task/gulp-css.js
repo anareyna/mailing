bdd1be46f5fb12fa9fe0cbc21106fab0f10724c2
Ana Reyna <ana.reyna@orbis.com.pe>
2017-06-08 16:46:46 -0500

    wip basepath css

--- gulp/task/gulp-css.js
+++ gulp/task/gulp-css.js
@@ -1,13 +1,17 @@
 var gulp       = require('gulp');
 var gulpStylus = require('gulp-stylus');
 
+var basePath   = process.cwd();
+
 gulp.task('css', function() {
-    return gulp.src([
-        './stylus/**/*.styl',
-        '!./stylus/_**/*.styl'
-        ], { base : './stylus' })
+    return gulp.src(['./stylus/**/*.styl', '!./stylus/_**/*.styl'], { base: basePath + '/stylus' })
       .pipe(gulpStylus({
           compress : false,
+          use: [
+            function(style) {
+                style.define("$pathBase", basePath)
+            }
+          ],
           linenos  : false,
           size: {
               title:'styles'
