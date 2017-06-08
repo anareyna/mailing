112eec83df35d6a47ea71cfaa964449e499c3fc2
Ana Reyna <ana.reyna@orbis.com.pe>
2017-06-08 15:24:24 -0500

    wip estructura de carpetas

--- gulp/task/gulp-css.js
+++ gulp/task/gulp-css.js
@@ -2,7 +2,7 @@ var gulp       = require('gulp');
 var gulpStylus = require('gulp-stylus');
 
 gulp.task('css', function() {
-    return gulp.src("./src/stylus/*.styl")
+    return gulp.src("./stylus/**/*.styl")
       .pipe(gulpStylus({
           compress : false,
           linenos  : false,
--- gulp/task/gulp-html.js
+++ gulp/task/gulp-html.js
@@ -1,13 +1,26 @@
 var gulp      = require('gulp');
 var pug       = require('gulp-pug');
 var inlineCss = require('gulp-inline-css');
+var through2  = require('through2');
+var fs        = require('fs');
+
+var basePath  = './';
 
 gulp.task('html', function() {
-    return gulp.src('./src/views/templates/**/*.pug', {base : "./src/"})
+    return gulp.src('./pug/modules/**/*.pug', { base : basePath})
         .pipe(pug({
             pretty  : true,
-            basedir : "./src/"
+            basedir : basePath
         }))
-        .pipe(inlineCss())
+        .pipe(inlineCss({
+            removeStyleTags: false
+        }))
+        .pipe(through2.obj(function(chunk, encoding, callback) {
+            var content      = chunk.contents.toString();
+            var cssContent = fs.readFileSync('./css/media-queries/media-queries.css').toString();
+            var newContent   = content.replace(/mediaQueries/gm, cssContent)
+            chunk.contents   = new Buffer(newContent, "utf-8");
+            callback(null, chunk);
+         }))
         .pipe(gulp.dest('./build'));
 });
